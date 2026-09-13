da6b3013c76a2448029fffea31da57107b96d211
mypc <mensethireach@gmail.com>
2026-09-13 23:10:00 +0700

    Set REQUEST_INTERVAL to 3.0s for guaranteed zero-429 continuous operation

--- distributed_supabase.py
+++ distributed_supabase.py
@@ -39,7 +39,7 @@ else:
     rand_suffix = "".join(random.choices(string.ascii_lowercase + string.digits, k=4))
     WORKER_ID = f"{hostname}-{rand_suffix}"
 
-REQUEST_INTERVAL = float(os.environ.get("REQUEST_INTERVAL", "2.2"))
+REQUEST_INTERVAL = float(os.environ.get("REQUEST_INTERVAL", "3.0"))
 
 CHARACTERS = string.ascii_letters + string.digits + "-_"
 INVALID_PHRASES = [
